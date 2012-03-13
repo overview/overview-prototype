681d54d8511e8263b91bf430d71fccf97c7aaa1a
Rhiannon Coppin <coppinr@gmail.com>
2012-03-13 13:28:15 -0700

    Removing -d32 flag. Shouldn't be needed.

--- overview.sh
+++ overview.sh
@@ -2,4 +2,4 @@
 BASEDIR=`dirname $0`
 CLASSPATH=$BASEDIR/visualization/lib
 
-java -classpath $CLASSPATH -d32 -XstartOnFirstThread -Xmx1500m -cp $CLASSPATH/.:$CLASSPATH/snappy.jar:$CLASSPATH/DJNativeSwing-SWT.jar:$CLASSPATH/DJNativeSwing.jar:$CLASSPATH/js.jar:$CLASSPATH/core.jar:$CLASSPATH/swt.jar:$CLASSPATH/opencsv-2.3.jar snappy.ui.Snappy -N $1-terms.vec -Z $1-featurenames.csv -U $1-urls.csv 
+java -classpath $CLASSPATH -XstartOnFirstThread -Xmx1500m -cp $CLASSPATH/.:$CLASSPATH/snappy.jar:$CLASSPATH/DJNativeSwing-SWT.jar:$CLASSPATH/DJNativeSwing.jar:$CLASSPATH/js.jar:$CLASSPATH/core.jar:$CLASSPATH/swt.jar:$CLASSPATH/opencsv-2.3.jar snappy.ui.Snappy -N $1-terms.vec -Z $1-featurenames.csv -U $1-urls.csv 
