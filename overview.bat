ab1d78836e5344f03db2f0633365de3e345a170d
jstray <jonathanstray@gmail.com>
2012-10-04 16:06:57 -0400

    Raise max memory to 6gb

--- overview.bat
+++ overview.bat
@@ -2,4 +2,4 @@
 set BASEDIR=%~dp0
 set CLASSPATH=%BASEDIR%visualization\lib
 
-java -classpath %CLASSPATH% -Xmx1500m -cp %CLASSPATH%;%CLASSPATH%\snappy.jar;%CLASSPATH%\DJNativeSwing-SWT.jar;%CLASSPATH%\DJNativeSwing.jar;%CLASSPATH%\js.jar;%CLASSPATH%\core.jar;%CLASSPATH%\windows\swt.jar;%CLASSPATH%\opencsv-2.3.jar snappy.ui.mainHolder -N %1-terms.vec -Z %1-featurenames.csv -U %1-urls.csv 
+java -classpath %CLASSPATH% -Xmx6000m -cp %CLASSPATH%;%CLASSPATH%\snappy.jar;%CLASSPATH%\DJNativeSwing-SWT.jar;%CLASSPATH%\DJNativeSwing.jar;%CLASSPATH%\js.jar;%CLASSPATH%\core.jar;%CLASSPATH%\windows\swt.jar;%CLASSPATH%\opencsv-2.3.jar snappy.ui.mainHolder -N %1-terms.vec -Z %1-featurenames.csv -U %1-urls.csv 
